dd60fba1610a25ed148f1d52617d4a3881aafc4c
Richard Wright <richard.wright@mightyhive.com>
2022-09-09 11:56:48 +1000

    v1

--- web.js
+++ web.js
@@ -15,17 +15,82 @@ app.post("/", function (req, res) {
   console.log(state);
 
   try {
+    var x;
+    var y;
+    var dir;
+    var dims = arena["dims"];
+    moves = ["F"];
     for (const player in state) {
       if (
         player == "https://cloud-run-hackathon-nodejs-hircheuosa-uc.a.run.app"
       ) {
-        console.log("ITS ME");
+        console.log("SETTING COORD");
+        var meObj = state[player];
+        x = meObj["x"];
+        y = meObj["y"];
+        dir = meObj["direction"];
+      }
+    }
+
+    for (const enemy in state) {
+      if (
+        enemy != "https://cloud-run-hackathon-nodejs-hircheuosa-uc.a.run.app"
+      ) {
+        //check if anyone is next to me
+        var enemyObj = state[enemy];
+        var ex = enemyObj["x"];
+        var ey = enemyObj["y"];
+        var edir = enemyObj["direction"];
+
+        if (dir == "N") {
+          if (edir == "S" && y == ey - 1) {
+            if (x != dims[0] - 1) {
+              moves = ["R"];
+            } else {
+              moves = ["L"];
+            }
+          } else if (y - ey >= 3) {
+            moves = ["T"];
+          }
+        }
+        if (dir == "S") {
+          if (edir == "N" && y == ey + 1) {
+            if (x != dims[0] - 1) {
+              moves = ["L"];
+            } else {
+              moves = ["R"];
+            }
+          } else if (ey - y >= 3) {
+            moves = ["T"];
+          }
+        }
+        if (dir == "E") {
+          if (edir == "W" && x == ex - 1) {
+            if (y != dims[1] - 1) {
+              moves = ["L"];
+            } else {
+              moves = ["R"];
+            }
+          } else if (ex - x >= 3) {
+            moves = ["T"];
+          }
+        }
+        if (dir == "W") {
+          if (edir == "E" && x == ex + 1) {
+            if (y != dims[1] - 1) {
+              moves = ["R"];
+            } else {
+              moves = ["L"];
+            }
+          } else if (x - ex >= 3) {
+            moves = ["T"];
+          }
+        }
       }
     }
   } catch (e) {
     console.log("error", e);
   }
-  const moves = ["F", "T", "L", "R"];
 
   // TODO add your implementation here to replace the random response
 
